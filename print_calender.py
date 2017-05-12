81228bd1778be0499cd3ccad72abf4e428b1b438
jsonmuchiri <jsonmuchiriwamuyu@gmail.com>
2017-05-12 14:53:54 +0300

    Printing the list of events of the user base on their input

---
+++ print_calender.py
@@ -0,0 +1,9 @@
+"""
+printing the list of the events for the user based on their input
+"""
+
+for x in output:
+        print(x)
+        for y in output[x]:
+            print(y,' : ', output[x][y])
+
